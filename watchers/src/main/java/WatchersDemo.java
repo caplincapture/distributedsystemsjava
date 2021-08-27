5f4b2548644c528bbeeaf4182325c8356deae6b6
caplincapture <leuping@gmail.com>
2021-08-27 15:03:23 -0700

    leader re-election

--- watchers/src/main/java/WatchersDemo.java
+++ watchers/src/main/java/WatchersDemo.java
@@ -22,41 +22,57 @@
  *  SOFTWARE.
  */
 
-import org.apache.zookeeper.*;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
 
 import java.io.IOException;
-import java.util.Collections;
 import java.util.List;
 
 /**
- * Zookeeper Client Threading Model & Zookeeper Java API
+ * Watchers, Triggers and Introduction to Failure Detection
+ * https://www.udemy.com/course/distributed-systems-cloud-computing-with-java
  */
-public class LeaderElection implements Watcher {
+public class WatchersDemo implements Watcher {
     private static final String ZOOKEEPER_ADDRESS = "localhost:2181";
     private static final int SESSION_TIMEOUT = 3000;
+    private static final String TARGET_ZNODE = "/target_znode";
     private ZooKeeper zooKeeper;
 
-    public static void main(String[] arg) throws IOException, InterruptedException, KeeperException {
-        LeaderElection leaderElection = new LeaderElection();
-
-        leaderElection.connectToZookeeper();
-        leaderElection.run();
-        leaderElection.close();
-        System.out.println("Disconnected from Zookeeper, exiting application");
+    public static void main(String[] args) throws InterruptedException, IOException, KeeperException {
+        WatchersDemo watchersDemo = new WatchersDemo();
+        watchersDemo.connectToZookeeper();
+        watchersDemo.watchTargetZnode();
+        watchersDemo.run();
+        watchersDemo.close();
     }
 
     public void connectToZookeeper() throws IOException {
         this.zooKeeper = new ZooKeeper(ZOOKEEPER_ADDRESS, SESSION_TIMEOUT, this);
     }
 
-    private void run() throws InterruptedException {
+    public void run() throws InterruptedException {
         synchronized (zooKeeper) {
             zooKeeper.wait();
         }
     }
 
-    private void close() throws InterruptedException {
-        this.zooKeeper.close();
+    public void close() throws InterruptedException {
+        zooKeeper.close();
+    }
+
+    public void watchTargetZnode() throws KeeperException, InterruptedException {
+        Stat stat = zooKeeper.exists(TARGET_ZNODE, this);
+        if (stat == null) {
+            return;
+        }
+
+        byte[] data = zooKeeper.getData(TARGET_ZNODE, this, stat);
+        List<String> children = zooKeeper.getChildren(TARGET_ZNODE, this);
+
+        System.out.println("Data : " + new String(data) + ", children : " + children);
     }
 
     @Override
@@ -71,6 +87,25 @@ public class LeaderElection implements Watcher {
                         zooKeeper.notifyAll();
                     }
                 }
+                break;
+            case NodeDeleted:
+                System.out.println(TARGET_ZNODE + " was deleted");
+                break;
+            case NodeCreated:
+                System.out.println(TARGET_ZNODE + " was created");
+                break;
+            case NodeDataChanged:
+                System.out.println(TARGET_ZNODE + " data changed");
+                break;
+            case NodeChildrenChanged:
+                System.out.println(TARGET_ZNODE + " children changed");
+                break;
+        }
+
+        try {
+            watchTargetZnode();
+        } catch (KeeperException e) {
+        } catch (InterruptedException e) {
         }
     }
 }
